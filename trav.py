286ac7c6e11ae3e2bf043ee6213efe2b2bb9372e
shubhodeep9 <shubhodeep9@gmail.com>
2015-07-07 02:38:37 +0530

    Heroku

--- trav.py
+++ trav.py
@@ -170,4 +170,5 @@ def logout():
     return redirect(url_for('landing'))
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port= port)
